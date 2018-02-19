5173e89ee62c8e43eec67c014ab289a520b2c76b
Melanie MacDonald <melmacd94@gmail.com>
2018-02-18 22:02:34 -0500

    random data

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var data            =   [10,20,30,40,50];
+//d3 random library
+var data=[];
+
+for(var i=0; i< 20; i++){
+    var num= Math.floor(Math.random()*50);
+    data.push(num);
+}
+
+console.log(data);
 
 d3.select('#chart')
     .selectAll('div')
@@ -8,6 +16,6 @@ d3.select('#chart')
     .attr('class','bar')
     .style('height', function(d){
     var height = d*3;
-    return d + 'px'
+    return height + 'px'
 })
 ;
