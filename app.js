4fd1e0fee1e558a75e6cf703b2678069b394dd63
Melanie MacDonald <melmacd94@gmail.com>
2018-02-18 21:49:57 -0500

    bar graph

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var data            =   [10,20,30,40,50];
+
+d3.select('#chart')
+    .selectAll('div')
+    .data(data)
+    .enter()
+    .append('div')
+    .attr('class','bar')
+    .style('height', function(d){
+    var height = d*3;
+    return d + 'px'
+})
+;
